50da5692b9c891b33ea774f37084d6d92abb90bd
liuzhibin <liuzhibin@ktkt.com>
2017-11-02 19:02:31 +0800

    add

---
+++ ssl/gen.sh
@@ -0,0 +1,55 @@
+#!/bin/zsh
+
+# 生成证书
+genCert() {
+    cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+    cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes
+    cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes admin-csr.json | cfssljson -bare admin
+    cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes  kube-proxy-csr.json | cfssljson -bare kube-proxy
+}
+
+# 生成token
+genToken() {
+    export BOOTSTRAP_TOKEN=$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ')
+    echo ${BOOTSTRAP_TOKEN},kubelet-bootstrap,10001,"system:kubelet-bootstrap" > token.csv
+}
+
+# 生成kubeconfig
+genConfig() {
+    kubectl config set-cluster kubernetes --certificate-authority=ca.pem  --embed-certs=true --server=https://172.16.221.131:6443 --kubeconfig=kubelet-bootstrap.kubeconfig
+
+    kubectl config set-credentials kubelet-bootstrap --token=${BOOTSTRAP_TOKEN} --kubeconfig=kubelet-bootstrap.kubeconfig
+
+    kubectl config set-context default  --cluster=kubernetes --user=kubelet-bootstrap  --kubeconfig=kubelet-bootstrap.kubeconfig
+
+    kubectl config use-context default --kubeconfig=kubelet-bootstrap.kubeconfig
+
+    kubectl config set-cluster kubernetes --certificate-authority=ca.pem --embed-certs=true --server=https://172.16.221.131:6443 --kubeconfig=kube-proxy.kubeconfig
+
+    kubectl config set-credentials kube-proxy --client-certificate=kube-proxy.pem  --client-key=kube-proxy-key.pem --embed-certs=true --kubeconfig=kube-proxy.kubeconfig
+
+    kubectl config set-context default --cluster=kubernetes  --user=kube-proxy --kubeconfig=kube-proxy.kubeconfig
+
+    kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+}
+
+
+main() {
+    genCert
+
+    genToken
+
+    genConfig
+
+    tar cf k8s.tar *.pem *.kubeconfig token.csv
+
+    for ip in $@
+    do
+        scp k8s.tar root@$ip:/etc/k8s/ssl
+        ssh root@$ip 'cd /etc/k8s/ssl && tar xf k8s.tar && mv *.kubeconfig token.csv ../ && rm k8s.tar'
+    done
+    
+    ls | grep -v json | grep -v gen.sh | xargs rm 
+}
+
+main 172.16.221.131
